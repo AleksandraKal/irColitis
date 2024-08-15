81f4c88cbf60d4b7e3c303469123920f05f7abaa
AleksandraKalinic <79557591+AleksandraKal@users.noreply.github.com>
2024-08-15 12:09:05 +1000

    updated

---
+++ thomas/cleaning.r
@@ -0,0 +1,22 @@
+library(pacman)
+
+p_load(Seurat, tidyverse, readxl, ggplot2, dplyr, stringr, gridExtra, grid, Matrix, anndata)
+
+# constPath <- paste(getwd(), "/thomas/constants.r", sep = "")
+# retrive the constants
+# source(constPath)
+
+# TODO: make this more constant
+path <- paste(getwd(), "/irColitis/data/thomas/", sep = "")
+files <- list.files(path, pattern = "(cd4|cd8).*\\.h5ad$", ignore.case = TRUE)
+files <- "GSE206298_ircolitis-blood-cd4.h5ad"
+
+
+meta.list <- list()
+matrix.list <- list()
+
+for (file in files) {
+    # TODO: fix to a nicer format
+    ad <- read_h5ad(paste(getwd(), "/data/thomas/", file, sep = ""))
+    matrix <- t(ad$X)
+}
